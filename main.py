6ea483de21a64fc132cec8c290ce49309ab9bcba
Ivan Cheng <wlchengivan@gmail.com>
2023-03-18 01:16:19 +0800

    return time

--- main.py
+++ main.py
@@ -17,12 +17,18 @@ stations = pd.read_csv("NewStation.csv", index_col="index")
 url= "https://rt.data.gov.hk/v1/transport/mtr/lrt/getSchedule?station_id="
 
 #for x in range(0, len(stations)):
-for x in range(60, 61):
+for x in range(56, 57):
     stationID = stations.iloc[x][0]
     nameEng = stations.iloc[x][1]
     nameChi = stations.iloc[x][2]
 
     req = requests.get(url + str(stationID))
 
-    print(req.json())
+    platformList = req.json()['platform_list']
 
+    for y in range(0, len(platformList)):
+        try:
+            print(platformList[y]['end_service_status'])
+        except:
+            detail = platformList[y]['route_list'][0]
+            print(detail['route_no'] + " " + detail['dest_ch'] + " " + detail['time_ch'])
